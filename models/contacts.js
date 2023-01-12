62329f3875096145c5c7bb3250251703e6c0be0c
Andrey_Schulz <andreygorb040693@gmail.com>
2023-01-12 19:16:46 +0200

    complite hw3

--- models/contacts.js
+++ models/contacts.js
@@ -1,52 +1,26 @@
-const fs = require("fs/promises");
+const { Contact } = require("./models");
 
 const listContacts = async () => {
-  const list = await fs.readFile("models/contacts.json", "utf8");
-  return JSON.parse(list);
+  return await Contact.find();
 };
 
 const getContactById = async (contactId) => {
-  const list = await listContacts();
-
-  return list.find((item) => item.id === contactId);
+  return await Contact.findById(contactId);
 };
 
 const removeContact = async (contactId) => {
-  const list = await listContacts();
-
-  const removeList = JSON.stringify(list.filter((x) => x.id !== contactId));
-
-  if (list.length !== removeList.length) {
-    const newList = await fs.writeFile("models/contacts.json", removeList);
-    // await writeContacts(removeList);
-    return true;
-  }
-  return false;
+  return await Contact.findByIdAndRemove({ _id: contactId });
 };
 
 const addContact = async (body) => {
-  const list = await listContacts();
-  const id = (+list[list.length - 1].id + 1).toString();
-  const writeContacts = fs
-    .readFile("models/contacts.json", "utf8")
-    .then((list) => {
-      const listContact = JSON.parse(list);
-      const newContact = JSON.stringify([...listContact, { id, ...body }]);
-      fs.writeFile("models/contacts.json", newContact);
-    });
-  // await writeContacts([...list, { id, ...body }]);
-  return { id, ...body };
+  return await Contact.create(body);
 };
 
 const updateContact = async (contactId, body) => {
-  const list = await listContacts();
-  const index = list.findIndex((x) => x.id === contactId);
-  if (index === -1) return null;
-  list[index] = { ...list[index], ...body };
-  const updatelist = JSON.stringify(list);
-  fs.writeFile("models/contacts.json", updatelist);
-  //
-  return list[index];
+  console.log("123123123123123123pp");
+  return await Contact.findByIdAndUpdate({ _id: contactId }, body, {
+    new: true,
+  });
 };
 
 module.exports = {
